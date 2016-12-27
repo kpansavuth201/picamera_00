119d24b389a5bcaed490e17745eada21931e6885
kpansavuth <kpansavuth@gmail.com>
2016-12-26 23:41:34 -0700

    first commit

---
+++ camera.py
@@ -0,0 +1,10 @@
+from time import time
+
+class Camera(object):
+	"""An emulated from jpg files"""
+
+	def __init__(self):
+		self.frames = [open(f + '.jpg', 'rb').read() for f in ['1','2','3']]
+	
+	def get_frame(self):
+		return self.frames[int(time()) % 3]
